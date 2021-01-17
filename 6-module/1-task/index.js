452059184b67d623bf689609597698ad75257396
Evgeniy Ziganshin <dante-da@mail.ru>
2021-01-17 20:02:59 +0600

    Модуль 6. Задача 1 - добавил решение

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -24,11 +24,65 @@
  *          age: 25,
  *          salary: '1000',
  *          city: 'Petrozavodsk'
- *      },ы
+ *      },
  *
  * @constructor
  */
 export default class UserTable {
   constructor(rows) {
+    this.elem = this.createUserTable(rows);
+  }
+
+  createThead(size) {
+    let thead = document.createElement('thead'),
+      tr = document.createElement('tr');
+    thead.append(tr);
+
+    for (let i = 0; i < size; i++) {
+      let th = document.createElement('th');
+      tr.append(th);
+    }
+
+    tr.cells[0].textContent = "Имя";
+    tr.cells[1].textContent = "Возраст";
+    tr.cells[2].textContent = "Зарплата";
+    tr.cells[3].textContent = "Город";
+    return thead;
+  }
+
+  createButton(){
+    let button = document.createElement('button');    
+      button.textContent = 'X';
+      button.onclick = () => button.parentElement.parentElement.remove();
+    return button;
+  }
+
+  createTbody(rows) {
+    let tbody = document.createElement('tbody');
+    let that = this;
+
+    rows.forEach(function (item) {
+      let tr = document.createElement('tr');
+      tbody.append(tr);
+
+      Object.values(item).forEach(function (value) {
+        let td = document.createElement('td');
+        tr.append(td);
+        td.textContent = value;
+      });
+      
+      let td = document.createElement('td');
+      tr.append(td);
+      td.append(that.createButton());
+      
+    });
+    return tbody;
+  }
+  
+  createUserTable(rows) {
+    let table = document.createElement('table');
+    table.append(this.createThead(Object.keys(rows[0]).length + 1));
+    table.append(this.createTbody(rows));
+    return table;
   }
 }
