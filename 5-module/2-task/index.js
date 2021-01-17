3b400ed951fbb29959a78836dbc2f39beb48dd03
Evgeniy Ziganshin <dante-da@mail.ru>
2021-01-17 17:12:00 +0600

    Модуль 5. Добавил решение задач 1,2,3

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,14 @@
 function toggleText() {
-  // ваш код...
+  const button = document.querySelector('.toggle-text-button');
+  const text = document.getElementById('text');
+
+  button.addEventListener('click', () => {
+
+    if (text.hidden == false) {
+      text.hidden = true;
+    } else {
+      text.hidden = false;
+    }
+
+  });
 }
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,45 @@
 function initCarousel() {
-  // ваш код...
+  const buttonArrowRight = document.querySelector('.carousel__arrow_right'),
+    buttonArrowLeft = document.querySelector('.carousel__arrow_left'),
+    carouselInner = document.querySelector('.carousel__inner'),
+    edgeRightPosition = 3,
+    edgeLeftPosition = 0,
+    offsetPosition = 1;
+  let carouselPosition = 0,
+    position = 0;
+
+  function changePosition(offset) {
+    carouselPosition = carouselPosition - offset * carouselInner.offsetWidth;
+    position = position + offset;
+    carouselInner.style.transform = `translateX(${carouselPosition}` + `px)`;
+  }
+
+  function displayButton(edgePosition, direction) {
+    if (position == edgePosition) {
+      direction.style.display = 'none';
+    } else {
+      buttonArrowRight.style.display = '';
+      buttonArrowLeft.style.display = '';
+    }
+  }
+
+  buttonArrowRight.addEventListener('click', () => {
+    changePosition(offsetPosition);
+    displayButton(edgeRightPosition, buttonArrowRight);
+  });
+
+  buttonArrowLeft.addEventListener('click', () => {
+    changePosition(-offsetPosition);
+    displayButton(edgeLeftPosition, buttonArrowLeft);
+  });
+
+  if (position == edgeRightPosition) {
+    buttonArrowRight.style.display = 'none';
+  }
+
+  if (position == edgeLeftPosition) {
+    buttonArrowLeft.style.display = 'none';
+  }
+
 }
+
